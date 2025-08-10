ad8055b541f0e1bb5f655e659b9e18342601bbce
Alyander <momoroot33@gmail.com>
2025-08-10 14:34:55 +0300

    Task: Винести чувствительную информацию в .properties

--- src/main/java/com/javarush/jira/JiraRushApplication.java
+++ src/main/java/com/javarush/jira/JiraRushApplication.java
@@ -11,7 +11,9 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 public class JiraRushApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
         SpringApplication.run(JiraRushApplication.class, args);
     }
 }
